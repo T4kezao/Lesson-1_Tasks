dbd4c231e1e73cc0541d569c3fe265ed5926f268
Ilya <sayex9001@mail.ru>
2022-09-19 13:57:46 +0300

    Done Task001

---
+++ Task_001/Program.cs
@@ -0,0 +1,19 @@
+﻿int a, b;
+ 
+    Console.WriteLine("Введите первое число: ");
+ 
+    a = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("Введите второе число: ");
+ 
+    b = Convert.ToInt32(Console.ReadLine());
+ 
+        if (a > b)
+
+            Console.WriteLine("Наибольшее число {0} Наименьшее число {1} " , a, b);
+            
+        else
+
+            Console.WriteLine("Наибольшее число {0} Наименьшее число {1}", b , a);
+
+        Console.ReadLine();
