58b754906601b1fcbbad28d00385c71668714c87
Ilya <sayex9001@mail.ru>
2022-09-19 15:35:04 +0300

    Done Task004

---
+++ Task_004/Program.cs
@@ -0,0 +1,9 @@
+﻿int N;
+
+Console.WriteLine("Введите необходимый диапозон: ");
+N = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+    for (i = 1; i <= N; i++)
+        if ((i % 2) == 0 )
+        Console.WriteLine("В этом диапозоне найдены четные числа числа: " + i);
+    Console.ReadLine();
