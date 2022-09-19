a5b04565ea5a1ef4c5a3329c3148a77b40b6983e
Ilya <sayex9001@mail.ru>
2022-09-19 14:07:39 +0300

    Done Task003

---
+++ Task_003/Program.cs
@@ -0,0 +1,14 @@
+﻿int a;
+
+    Console.WriteLine("Введите число: ");
+
+    a = Convert.ToInt32(Console.ReadLine());
+
+    if ((a % 2) == 0)
+
+        Console.WriteLine("Введеное число - четное!");
+    else
+
+        Console.WriteLine("Введеное число - нечетное!");
+
+    Console.ReadLine();   
