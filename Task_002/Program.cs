47edbb8e230c5deea7c5207d490ce7a94f3182fa
Ilya <sayex9001@mail.ru>
2022-09-19 13:58:10 +0300

    Done Task002

---
+++ Task_002/Program.cs
@@ -0,0 +1,12 @@
+﻿int a, b, c;
+
+    Console.WriteLine("Введите три числа: ");
+    a = Convert.ToInt32(Console.ReadLine());
+    b = Convert.ToInt32(Console.ReadLine());
+    c = Convert.ToInt32(Console.ReadLine());
+
+    int maximal = Math.Max(Math.Max(a, b), c);
+
+    Console.WriteLine("Наибольшее число {0}", maximal);
+
+    Console.ReadLine();
